32fbb42a25f999e7f292e448be795cc8d082498f
castroa <castro@baxenergy.com>
2017-09-18 16:42:57 +0200

    Added API stub

---
+++ ServiceAPI/ServiceAPI/Dal/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAPI.Dal
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
--- ServiceAPI/ServiceAPI/ServiceApiController.cs
+++ ServiceAPI/ServiceAPI/ServiceApiController.cs
@@ -1,23 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAPI.Dal;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace ServiceAPI
 {
     [Route("api")]
     public class ServiceApiController : Controller
     {
-        [HttpGet("getStudents")]
+        static List<Student> students = new List<Student>(new[] { new Student { Id = 1, Name = "Alberto", DateOfBirth = new DateTime(1989, 5, 4) } });
+
+        [HttpGet("students")]
         public async Task<IActionResult> GetStudents()
         {
             await Task.Delay(100); //Simulate work
 
-            return Ok(new dynamic[]
-            {
-                new { Name = "Alberto", Age=28 }
-            });
+            return Ok(students);
+        }
+
+        [HttpGet("student")]
+        public async Task<IActionResult> GetStudent([FromQuery]int id)
+        {
+            return Ok(students.FirstOrDefault(s => s.Id == id));
+        }
+
+        [HttpPut("students")]
+        public async Task<IActionResult> CreateStudent([FromBody]Student student)
+        {
+            students.Add(student);
+
+            return Ok();
+        }
+
+        [HttpPost("students")]
+        public async Task<IActionResult> UpdateStudent([FromBody]Student student)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("students")]
+        public async Task<IActionResult> DeleteStudent([FromQuery]int id)
+        {
+            students.RemoveAll(s => s.Id == id);
+            return Ok();
         }
     }
 }
